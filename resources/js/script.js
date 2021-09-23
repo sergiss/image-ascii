8d97555092f32a648ff08958ae02454582861f88
Sergio Soriano <sergi.ss4@gmail.com>
2021-09-23 02:06:59 +0200

    First Commit

---
+++ resources/js/script.js
@@ -0,0 +1,77 @@
+var image = "";
+
+window.onload = function () {
+  let input = document.querySelector("#file");
+  input.onchange = function (e) {
+    let file = e.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.onload = function () {
+      loadImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
+  input = document.querySelector("#save");
+  input.addEventListener("click", function (e) {
+
+    let link = document.createElement("a");
+    link.setAttribute("href", 'data:text/plain;charset=utf-8,' + encodeURIComponent(image));
+    link.setAttribute("download", "image.txt");
+    link.style.display = 'none';
+    document.body.appendChild(link);
+
+    link.click();
+    document.body.removeChild(link);
+
+  });
+
+  loadImage("logo.gif");
+}
+
+loadImage = function (src) {
+  let img = document.createElement("img");
+  img.onload = function () {
+    handleImage(img);
+  };
+  img.src = src;
+}
+
+handleImage = function (img) {
+  let ascii = "#####********+++++++++=========--------:::::::::..";
+
+  let w = 320;
+  let h = 320;
+  
+  let canvas1 = document.querySelector("#canvas");
+  canvas1.width = w;
+  canvas1.height = h;
+  let ctx1 = canvas1.getContext("2d");
+  ctx1.font = "5px monospace";
+
+  let canvas2 = document.createElement("canvas");
+  canvas2.width = w;
+  canvas2.height = h;
+  let ctx2 = canvas2.getContext("2d");
+  ctx2.scale((w / img.width) * 0.5, (h / img.height) * 0.5);
+  ctx2.drawImage(img, 0, 0);
+
+  let data = ctx2.getImageData(0, 0, w, h).data;
+  let i, j, r, g, b, grayscale, index;
+  
+  image = "";
+  for (j = 0; j < w >> 1; j++) {
+    for (i = 0; i < h >> 1; i++) {    
+      let off = (j * w + i) << 2;
+      r = data[off];
+      g = data[off + 1];
+      b = data[off + 2];
+      grayscale = (r + g + b) / 3.0;
+      index = Math.floor((grayscale / 255) * ascii.length);
+      let char = ascii.charAt(index);
+      ctx1.fillText(char, i * 2, j * 2);
+      image += char;
+    }
+    image += '\n';
+  }
+
+}
